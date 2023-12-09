42aafdd93c8f4b143e7c48810caeed19483314dc
Dhaval Patel <dhaval.contributor@gmail.com>
2023-12-09 14:39:07 +0530

    tailwind config added

---
+++ apps/web/tailwind.config.ts
@@ -0,0 +1,12 @@
+/**  @type {import('tailwindcss').Config} */
+
+const sharedConfig = require("@repo/config-tailwind");
+module.exports = {
+  ...sharedConfig,
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "../../packages/ui/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+};
